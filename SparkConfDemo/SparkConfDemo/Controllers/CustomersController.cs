b1ab2a4f25a692615d1a278be686bae9eb860d76
Richard L. Taylor Jr. <rtaylor@rightincode.com>
2015-06-04 16:50:29 -0400

    Created basic Web API that returns a list of customers

---
+++ SparkConfDemo/SparkConfDemo/Controllers/CustomersController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using SparkConfDemo.Models;
+
+namespace SparkConfDemo.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<Customer> Get()
+        {
+            Customer cust1 = new Customer
+            {
+                FirstName = "Richard",
+                LastName = "Taylor",
+                Email = "rtaylor@rightincode.com",
+                Phone = "704-777-1927"
+            };
+
+            Customer cust2 = new Customer
+            {
+                FirstName = "David",
+                LastName = "Taylor",
+                Email = "dtaylor@rightincode.com",
+                Phone = "704-777-1928"
+            };
+
+            List<Customer> customers = new List<Customer> {cust1, cust2};
+
+            return customers;
+        }
+
+        //// GET api/values/5
+        //public string GetById(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ SparkConfDemo/SparkConfDemo/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace SparkConfDemo.Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
